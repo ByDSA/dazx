61858e775f60cb680c7b668b26cde620025735d7
Daniel Sales <danisales.es@gmail.com>
2023-05-27 10:45:47 +0200

    Añadir opción de transformación de path a index.js

--- src/dsa/transform.ts
+++ src/dsa/transform.ts
@@ -1,12 +1,35 @@
 export function transformScript(source: string) {
   const NODE_PATH = process.env.NODE_PATH
 
-  let modifiedContent = source
-  ;['datest', 'bash', 'rclone'].forEach((name) => {
+  let modifiedContent = source;
+  modifiedContent = replaceDSAJs(modifiedContent,"datest", "rclone");
+  modifiedContent = replaceDSAIndex(modifiedContent, "bash");
+
+  return modifiedContent
+}
+
+function replaceDSAJs(modifiedContent: string, ...cases: string[]) {
+  const NODE_PATH = process.env.NODE_PATH;
+
+  cases.forEach((name) => {
     modifiedContent = modifiedContent.replace(
       'from "dazx/' + name + '";',
       'from "' + NODE_PATH + '/dazx/build/dsa/' + name + '.js";'
     )
-  })
-  return modifiedContent
+  });
+
+  return modifiedContent;
+}
+
+function replaceDSAIndex(modifiedContent: string, ...cases: string[]) {
+  const NODE_PATH = process.env.NODE_PATH;
+
+  cases.forEach((name) => {
+    modifiedContent = modifiedContent.replace(
+      'from "dazx/' + name + '";',
+      'from "' + NODE_PATH + '/dazx/build/dsa/' + name + '/index.js";'
+    )
+  });
+
+  return modifiedContent;
 }
