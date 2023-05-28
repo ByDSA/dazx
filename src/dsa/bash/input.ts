929f650bdfe814e9f4dc9a811c28432a00908136
Daniel Sales <danisales.es@gmail.com>
2023-05-28 11:23:13 +0200

    Actualizar questionYN

--- src/dsa/bash/input.ts
+++ src/dsa/bash/input.ts
@@ -1,7 +1,7 @@
 import { question } from "../../goods.js";
 
 export async function questionYNAsync(questionStr: string) {
-  const answer = await question(questionStr, { choices: ['y', 'n'] });
+  const answer = await question(questionStr + " (y/n): ", { choices: ['y', 'n'] });
 
   return answer.toLowerCase() === 'y';
 }
