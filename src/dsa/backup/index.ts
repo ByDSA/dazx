f2e5ddd1a4c568950817ff2e1bf90a86ea039b1f
Daniel Sales <danisales.es@gmail.com>
2023-05-28 15:48:42 +0200

    Añadir función backup genérica

--- src/dsa/backup/index.ts
+++ src/dsa/backup/index.ts
@@ -1,17 +1,54 @@
+import path from "path";
 import { $ } from "../../core.js";
 import { isInstalledAsync } from "../bash/index.js";
 import { forceSudoAsync } from "../bash/sudo.js";
 
-export async function backupThisFolderAsync() {
-  await forceSudoAsync();
-
-  await forceBackupInstalledAsync();
-
-  await $`sudo backup -c -t iso ${__dirname}`;
+type BackupThisFolderParams = Omit<BackupParams, "fromFolder">;
+export async function backupThisFolderAsync(params?: BackupThisFolderParams) {
+  return backupAsync({
+    fromFolder: __dirname,
+    ...params
+  });
 }
 
 export async function forceBackupInstalledAsync() {
   if (!await isInstalledAsync("backup")) {
     throw new Error("Not Implemented");
+  }
+}
+
+type BackupParams = {
+  type?: "iso";
+  fromFolder: string;
+  toFolder?: string;
+  check?: boolean;
+  deleteAfter?: boolean;
+};
+export async function backupAsync(params: BackupParams) {
+  const { type = "iso", fromFolder, toFolder, check = false, deleteAfter = false } = params;
+
+  if (!fromFolder)
+    throw new Error("fromFolder is required");
+
+  await forceSudoAsync();
+
+  await forceBackupInstalledAsync();
+
+  const flags = [];
+
+  if (check)
+    flags.push("--checkAfter");
+
+  if (deleteAfter)
+    flags.push("--deleteAfter");
+
+  await $`sudo backup ${flags} -t ${type} ${fromFolder}`;
+
+  if (toFolder) {
+    const parentFromFolder = path.join(fromFolder, "..");
+    const backupFile = (await $`cd ${parentFromFolder} && echo $(ls | grep "${path.basename(fromFolder)}" | grep ".iso")`).stdout.trim();
+    const origin = path.resolve(".", parentFromFolder, backupFile);
+    const to = path.resolve(toFolder) + "/";
+    await $`sudo mv ${origin} ${to}`;
   }
 }
