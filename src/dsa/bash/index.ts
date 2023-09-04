498741b868517450676507e959827be16fa2cc51
Daniel Sales <danisales.es@gmail.com>
2023-09-04 17:05:10 +0200

    feat: función para cargar variables de entorno desde archivo

--- src/dsa/bash/index.ts
+++ src/dsa/bash/index.ts
@@ -1,6 +1,6 @@
 
 import { $ } from '../../core.js';
-import { which } from '../../goods.js';
+import { fs, which } from '../../goods.js';
 
 export * from "./input.js";
 export * from "./sudo.js";
@@ -30,4 +30,30 @@ export async function dirSizeAsync(directory: string) {
   $.verbose = verboseTmp;
 
   return ret;
+}
+
+async function loadEnv(pathFile: string) {
+  console.log(`Loading env file ${pathFile}`);
+
+  const envFile = fs.readFileSync(pathFile);
+  const envFileLines = envFile.toString().split("\n");
+  const envFileLinesFiltered = envFileLines.filter((line: string) => {
+    const trimmedLine = line.trim();
+
+    return trimmedLine.length > 0 && !trimmedLine.startsWith("#");
+  } );
+  const envVars = envFileLinesFiltered.reduce((acc: {[key: string]: string}, line: string) => {
+    const [key, ...valueSplited] = line.split("=");
+    const value = valueSplited.join("=");
+
+    acc[key] = value;
+
+    return acc;
+  }, {
+  } );
+
+  process.env = {
+    ...process.env,
+    ...envVars,
+  };
 }
