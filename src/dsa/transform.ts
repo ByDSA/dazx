bdecb3351a99016f8c0a16987b604660915b5ce9
Daniel Sales <danisales.es@gmail.com>
2023-12-12 16:46:55 +0100

    fix: encontrar archivo cuando NODE_PATH es múltiple

--- src/dsa/transform.ts
+++ src/dsa/transform.ts
@@ -1,6 +1,6 @@
-export function transformScript(source: string) {
-  const NODE_PATH = process.env.NODE_PATH
+import { fs, path } from "../index.js";
 
+export function transformScript(source: string) {
   let modifiedContent = source;
   modifiedContent = replaceDSAJs(modifiedContent,"datest", "rclone");
   modifiedContent = replaceDSAIndex(modifiedContent, "bash", "backup", "node", "git", "fs", "mongo");
@@ -8,13 +8,31 @@ export function transformScript(source: string) {
   return modifiedContent
 }
 
-function replaceDSAJs(modifiedContent: string, ...cases: string[]) {
+function findInNodePath(file: string): string {
   const NODE_PATH = process.env.NODE_PATH;
 
+  if (!NODE_PATH) throw new Error("NODE_PATH not defined");
+
+  const nodePaths = NODE_PATH.split(":");
+
+  for (const candidateNodePath of nodePaths) {
+    const candidateFile = path.join(candidateNodePath,file);
+
+    if (fs.existsSync(candidateFile)) {
+      return candidateFile;
+    }
+  }
+
+  throw new Error(`File ${file} not found in NODE_PATH`);
+}
+
+function replaceDSAJs(modifiedContent: string, ...cases: string[]) {
   cases.forEach((name) => {
+    const newFile = findInNodePath(`dazx/build/dsa/${name}.js`);
+
     modifiedContent = modifiedContent.replaceAll(
       'from "dazx/' + name + '";',
-      'from "' + NODE_PATH + '/dazx/build/dsa/' + name + '.js";'
+      'from "' + newFile + '";'
     )
   });
 
@@ -22,12 +40,11 @@ function replaceDSAJs(modifiedContent: string, ...cases: string[]) {
 }
 
 function replaceDSAIndex(modifiedContent: string, ...cases: string[]) {
-  const NODE_PATH = process.env.NODE_PATH;
-
   cases.forEach((name) => {
+    const newFile = findInNodePath(`dazx/build/dsa/${name}/index.js`);
     modifiedContent = modifiedContent.replaceAll(
       'from "dazx/' + name + '";',
-      'from "' + NODE_PATH + '/dazx/build/dsa/' + name + '/index.js";'
+      'from "' + newFile + '";'
     )
   });
 
